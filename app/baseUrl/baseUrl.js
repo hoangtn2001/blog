0d3379dc3651f9fec15f410fbed0144c72ca9ec1
hoangtn2001 <bachnhan11a9@gmail.com>
2026-01-14 13:24:19 +0700

    fix 13

--- app/baseUrl/baseUrl.js
+++ app/baseUrl/baseUrl.js
@@ -1,7 +1,8 @@
-export function getBaseUrl() {
+const getBaseUrl = () => {
   if (typeof window !== "undefined") {
     return "";
   }
 
   return process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000";
-}
+};
+export default getBaseUrl;
