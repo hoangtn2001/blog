75017f69498cb749e117c7fbd0e8dda80d31a54a
hoangtn2001 <bachnhan11a9@gmail.com>
2026-01-14 11:13:22 +0700

    fix doctype

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,29 +1,36 @@
 import prisma from "@/utils/prisma";
 import { NextResponse } from "next/server";
 
-export const GET = async (req, context) => {
-  const params = await context.params;
-
+export async function GET(
+  req: Request,
+  { params }: { params: { slug: string } }
+) {
   const { slug } = params;
+
   try {
-    // Lấy lại post sau khi tăng views
     const post = await prisma.post.findUnique({
       where: { slug },
+      include: {
+        user: true,
+        cat: true,
+      },
     });
-    const updatedPost = await prisma.post.update({
-      where: { slug },
-      data: { views: post.views + 1 },
-    });
+
     if (!post) {
       return NextResponse.json({ message: "Post not found" }, { status: 404 });
     }
 
+    await prisma.post.update({
+      where: { slug },
+      data: { views: post.views + 1 },
+    });
+
     return NextResponse.json(post, { status: 200 });
   } catch (err) {
-    console.error(err);
+    console.error("POST SLUG API ERROR:", err);
     return NextResponse.json(
       { message: "Something went wrong!" },
       { status: 500 }
     );
   }
-};
+}
